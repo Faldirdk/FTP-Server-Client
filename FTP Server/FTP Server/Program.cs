d9d4e065f921109d300f505df808374c27cf5a4c
Søren <soeren.m.n@hotmail.com>
2016-11-03 14:21:43 +0100

    KJkjzsdlkfj

--- FTP Server/FTP Server/Program.cs
+++ FTP Server/FTP Server/Program.cs
@@ -93,7 +93,9 @@ namespace FTP_Server
                     s.Send(new byte[] { 0x06 });
                     break;
                 default:
+                    
                     File.WriteAllBytes(filNavn, df);
+                    
                     break;
             }
             
